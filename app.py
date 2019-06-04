c5c71f78ada4742a0d6e0dc5582d7555fc7446ba
shubhamjayawant <srjdbest@gmail.com>
2019-06-04 16:23:33 -0400

    Working GET, POST & PUT API endpoints

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 from flask import request, jsonify
+from flask_api import status
 import pprint
 import json
 
@@ -17,11 +19,62 @@ def notes():
 
     if request.method == 'POST':
 
-        return jsonify(createNote(request.data))
+        return createNote(request.data)
 
     else:
 
-        return jsonify(getNoteList())
+        return getNoteList()
+
+@app.route('/notes/<string:noteID>', methods=['GET', 'PUT'])
+def noteSpecific(noteID):
+
+    if request.method == 'GET':
+
+        return getNote(noteID)
+
+    else:
+
+        return updateNote(noteID, request.data)
+
+def getNote(noteID):
+
+    result = None
+
+    try:
+
+        result = db.notes.find_one({'_id' : ObjectId(noteID)}, {'title':1, 'body' : 1, '_id' : 0})
+
+    except:
+
+        pass
+
+    if result:
+
+        return createResponse({'data': result}, 200)
+
+    else:
+
+        return createResponse({'data': 'Object not found'}, 404)
+
+def updateNote(noteID, note):
+
+    note = json.loads(note.decode('utf8').replace("'", '"'))
+
+    if invalidData(note):
+
+        return createResponse({'data' : 'Invalid data posted'}, 400)
+
+    else:
+
+        result = db.notes.find_one_and_replace({'_id': ObjectId(noteID)}, note, {'title':1, 'body' : 1, '_id' : 0})
+
+        if result:
+
+            return createResponse({'data' : result}, 201)
+
+        else:
+
+            return createResponse({'data': 'Object not found'}, 404)
 
 def createNote(note):
 
@@ -29,11 +82,11 @@ def createNote(note):
 
     if invalidData(note):
 
-        return 'invalid data posted'
+        return createResponse({'data': 'Invalid data posted'}, 400)
 
     else:
 
-        return str(db.notes.insert_one(note).inserted_id)
+        return createResponse({'data': str(db.notes.insert_one(note).inserted_id)}, 201)
 
 def invalidData(data):
 
@@ -57,12 +110,17 @@ def getNoteList():
 
     noteList = []
 
-    for note in db.notes:
+    for note in db.notes.find({}, {'title':1}):
 
-        pprint.pprint(note)
-        # noteList.append({'id' : note['id'], 'title' : note['title']})
+        noteList.append({'id' : str(note['_id']), 'title' : note['title']})
 
-    return noteList
+    return createResponse({'data': noteList}, 200)
+
+def createResponse(data, statusCode):
+
+    response = jsonify(data)
+    response.status_code = statusCode
+    return response
 
 
 if __name__ == '__main__':
